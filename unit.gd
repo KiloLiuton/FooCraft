ac81ef417fb9d4f54d3902c97f08e8d900fb10de
Kevin Liu Rodrigues <kkevin.liu@gmail.com>
2020-10-20 10:25:55 -0300

    add visual drawing of the path

--- unit.gd
+++ unit.gd
@@ -5,11 +5,17 @@ var path_node = 0
 var speed = 10
 var begin = Vector3()
 var end = Vector3()
+var draw_path = true
 
 onready var nav = get_parent()
 
+var m = SpatialMaterial.new()
+
 func _ready():
-	pass # Replace with function body.
+	set_process_input(true)
+	m.flags_unshaded = true
+	m.flags_use_point_size = true
+	m.albedo_color = Color.white
 
 func _physics_process(delta):
 	if path_node < path.size():
@@ -22,17 +28,30 @@ func _physics_process(delta):
 func move_to(target_pos):
 	path = nav.get_simple_path(global_transform.origin, target_pos)
 	path_node = 0
+	
+	if draw_path:
+		var im = get_node("../../draw")
+		im.set_material_override(m)
+		im.clear()
+		im.begin(Mesh.PRIMITIVE_POINTS, null)
+		im.add_vertex(begin)
+		im.add_vertex(end)
+		im.end()
+		im.begin(Mesh.PRIMITIVE_LINE_STRIP, null)
+		for x in path:
+			im.add_vertex(x)
+		im.end()
 
 func _unhandled_input(event):
 
 	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.pressed:
-		print("FOO")
 		var raylength = 200
 		var camera = get_node("../../CameraBase/Camera")
 		var rayfrom = camera.project_ray_origin(event.position)
-		var rayto = rayfrom + camera.project_ray_normal(event.position) * raylength
-		var p = nav.get_closest_point_to_segment(rayfrom, rayto)
+		var raydir = rayfrom + camera.project_ray_normal(event.position) * raylength
+		var p = nav.get_closest_point_to_segment(rayfrom, raydir)
 		
 		begin = nav.get_closest_point(get_translation())
 		end = p
 		move_to(p)
+		print("Walking to: ", end)
